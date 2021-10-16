ae6ae27db82dfc8aa53a662f46de538e09ac76d2
Andrew Mogensen <andrewmogensen70@gmail.com>
2021-10-16 12:07:49 -0500

    create and update Poll

--- src/main/kotlin/com/rankchoice/vote/service/PollService.kt
+++ src/main/kotlin/com/rankchoice/vote/service/PollService.kt
@@ -42,20 +42,73 @@ class PollService @Autowired constructor(
                 id = UUID.randomUUID(),
                 name = pollRequest.name,
                 description = pollRequest.description,
-                ownerId = UUID.randomUUID(),
+                ownerId = pollRequest.ownerId,
                 status = PollStatus.Scheduled,
                 startTime = pollRequest.startTime ?: Instant.now(),
                 endTime = pollRequest.endTime,
                 options = pollRequest.options
             )
+            validatePoll(pollEntity)
+
             val savedPoll: Poll = pollRepository.save(pollEntity)
             ResponseEntity(savedPoll.id.toString(), HttpStatus.CREATED)
+
         } catch (dae: DataAccessException) {
             log.error(dae) { "Failed to save Poll '${pollRequest.name}'" }
             ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
         } catch (iae: IllegalArgumentException) {
             log.error(iae) { "Bad request to save Poll '${pollRequest.name}'" }
             ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    fun updatePoll(pollRequest: PollRequest, id: UUID): ResponseEntity<String> {
+        return try {
+            val existingPoll = pollRepository.findByIdOrNull(id)
+            existingPoll?.let {
+                if (existingPoll.status != PollStatus.Scheduled) {
+                    ResponseEntity("Poll cannot be updated after polling starts.", HttpStatus.FORBIDDEN)
+                } else {
+                    val pollEntity = Poll(
+                        id = existingPoll.id,
+                        name = pollRequest.name,
+                        description = pollRequest.description,
+                        ownerId = existingPoll.ownerId,
+                        status = PollStatus.Scheduled,
+                        startTime = pollRequest.startTime ?: existingPoll.startTime,
+                        endTime = pollRequest.endTime,
+                        options = pollRequest.options
+                    )
+                    validatePoll(pollEntity)
+                    val savedPoll: Poll = pollRepository.save(pollEntity)
+                    ResponseEntity(savedPoll.id.toString(), HttpStatus.OK)
+                }
+            } ?: ResponseEntity(HttpStatus.NOT_FOUND)
+        } catch (dae: DataAccessException) {
+            log.error(dae) { "Failed to access Poll '$id'" }
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        } catch (iae: IllegalArgumentException) {
+            log.error(iae) { "Bad request for Poll '$id'" }
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    private fun validatePoll(poll: Poll) {
+        // TODO: use custom exception type
+        if (poll.endTime <= poll.startTime) {
+            throw IllegalArgumentException("endTime must be after startTime")
+        }
+        if (poll.name.isBlank()) {
+            throw IllegalArgumentException("name cannot be empty")
+        }
+        if (poll.description.isBlank()) {
+            throw IllegalArgumentException("description cannot be empty")
+        }
+        if (poll.ownerId.toString().isBlank()) {
+            throw IllegalArgumentException("owner cannot be empty")
+        }
+        if (poll.ownerId.toString().isBlank()) {
+            throw IllegalArgumentException("owner cannot be empty")
         }
     }
 }
--- src/test/kotlin/com/rankchoice/vote/service/PollServiceTest.kt
+++ src/test/kotlin/com/rankchoice/vote/service/PollServiceTest.kt
@@ -5,15 +5,20 @@ import com.rankchoice.vote.entity.PollOption
 import com.rankchoice.vote.entity.PollRequest
 import com.rankchoice.vote.entity.PollStatus
 import com.rankchoice.vote.repository.PollRepository
+import io.mockk.Called
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.springframework.dao.DataAccessResourceFailureException
 import org.springframework.dao.DataRetrievalFailureException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class PollServiceTest {
     private val pollRepo = mockk<PollRepository>()
@@ -37,7 +42,7 @@ internal class PollServiceTest {
                 PollOption("opt2", "desc2")
             )
         )
-        every { pollRepo.findById(id) } returns Optional.of(expectedPoll)
+        every { pollRepo.findByIdOrNull(id) } returns expectedPoll
 
         val result = pollService.findPoll(id)
 
@@ -49,7 +54,7 @@ internal class PollServiceTest {
     @Test
     fun findPollNotFound() {
         val id = UUID.randomUUID()
-        every { pollRepo.findById(id) } returns Optional.empty()
+        every { pollRepo.findByIdOrNull(id) } returns null
 
         val result = pollService.findPoll(id)
 
@@ -59,7 +64,7 @@ internal class PollServiceTest {
     @Test
     fun findPollIllegalArgument() {
         val id = UUID.randomUUID()
-        every { pollRepo.findById(id) } throws IllegalArgumentException("test iae")
+        every { pollRepo.findByIdOrNull(id) } throws IllegalArgumentException("test iae")
 
         val result = pollService.findPoll(id)
 
@@ -69,7 +74,7 @@ internal class PollServiceTest {
     @Test
     fun findPollDataInaccessible() {
         val id = UUID.randomUUID()
-        every { pollRepo.findById(id) } throws DataRetrievalFailureException("test dae")
+        every { pollRepo.findByIdOrNull(id) } throws DataRetrievalFailureException("test dae")
 
         val result = pollService.findPoll(id)
 
@@ -103,22 +108,272 @@ internal class PollServiceTest {
             endTime = endTime,
             options = request.options
         )
-//        TODO: Figure out complex mock matching
-//        every { pollRepo.save(any()) } returns expectedPoll
-//
-//        val result = pollService.createPoll(request)
-//
-//        assertEquals(HttpStatus.OK, result.statusCode)
-//        assertEquals(expectedPoll.id, result.body)
+        every { pollRepo.save<Poll>(any()) } returns expectedPoll
+
+        val result = pollService.createPoll(request)
+
+        verify {
+            pollRepo.save<Poll>(withArg {
+                TestUtilities.validateIdReplaced(expectedPoll, it, "id")
+            })
+        }
+        assertEquals(HttpStatus.CREATED, result.statusCode)
+        assertEquals(id.toString(), result.body)
     }
 
     @Test
-    fun createPollBadRequest() {
-        // TODO
+    fun createPollBadRequestOnSave() {
+        val request = PollRequest(
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = Instant.now(),
+            endTime = Instant.now().plus(5, ChronoUnit.DAYS),
+            options = listOf(
+                PollOption("opt1", "desc1"),
+                PollOption("opt2", "desc2")
+            )
+        )
+        every { pollRepo.save<Poll>(any()) } throws IllegalArgumentException("test iae")
+
+        val result = pollService.createPoll(request)
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty())
     }
 
     @Test
-    fun createPollDataInaccessible() {
-        // TODO
+    fun createPollDataInaccessibleOnSave() {
+        val request = PollRequest(
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = Instant.now(),
+            endTime = Instant.now().plus(5, ChronoUnit.DAYS),
+            options = listOf(
+                PollOption("opt1", "desc1"),
+                PollOption("opt2", "desc2")
+            )
+        )
+        every { pollRepo.save<Poll>(any()) } throws DataAccessResourceFailureException("test dae")
+
+        val result = pollService.createPoll(request)
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty())
+    }
+
+    @Test
+    fun createPollValidateEndDate() {
+        val request = PollRequest(
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = null,
+            endTime = Instant.now(),
+            options = listOf(
+                PollOption("opt1", "desc1"),
+                PollOption("opt2", "desc2")
+            )
+        )
+
+        val result = pollService.createPoll(request)
+
+        verify { pollRepo wasNot Called }
+        assertEquals(HttpStatus.BAD_REQUEST, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty()) // TODO: useful validation messages
+    }
+
+    @Test
+    fun updatePollSuccess() {
+        val ownerId = UUID.randomUUID()
+        val ownerIdNew = UUID.randomUUID()
+        val id = UUID.randomUUID()
+        val startTime = Instant.now()
+        val endTime = Instant.now().plus(4, ChronoUnit.DAYS)
+        val endTimeNew = endTime.plus(1, ChronoUnit.DAYS)
+        val request = PollRequest(
+            name = "new test poll",
+            description = "new description for test poll",
+            ownerId = ownerIdNew,
+            startTime = null,
+            endTime = endTimeNew,
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        val existingPoll = Poll(
+            id = id,
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = ownerId,
+            status = PollStatus.Scheduled,
+            startTime = startTime,
+            endTime = endTime,
+            options = emptyList()
+        )
+        val expectedPoll = Poll(
+            id = id,
+            name = "new test poll", // name gets updated
+            description = "new description for test poll", // description gets updated
+            ownerId = ownerId, // ownerId does not get updated
+            status = PollStatus.Scheduled, // status is still scheduled
+            startTime = startTime, // startTime uses existing, because request has null startTime
+            endTime = endTimeNew, // endTime gets updated
+            options = request.options // options get updated
+        )
+        every { pollRepo.findByIdOrNull(id) } returns existingPoll
+        every { pollRepo.save<Poll>(any()) } returns expectedPoll
+
+        val result = pollService.updatePoll(request, id)
+
+        verify {
+            pollRepo.save<Poll>(withArg {
+                TestUtilities.validateIdReplaced(expectedPoll, it, "id")
+            })
+        }
+        assertEquals(HttpStatus.OK, result.statusCode)
+        assertEquals(id.toString(), result.body)
+    }
+
+    @Test
+    fun updatePollNotFound() {
+        val id = UUID.randomUUID()
+        val request = PollRequest(
+            name = "new test poll",
+            description = "new description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = Instant.now(),
+            endTime = Instant.now().plus(5, ChronoUnit.DAYS),
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        every { pollRepo.findByIdOrNull(id) } returns null
+
+        val result = pollService.updatePoll(request, id)
+
+        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty())
+    }
+
+    @Test
+    fun updatePollBadRequest() {
+        val id = UUID.randomUUID()
+        val request = PollRequest(
+            name = "new test poll",
+            description = "new description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = Instant.now(),
+            endTime = Instant.now().plus(5, ChronoUnit.DAYS),
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        every { pollRepo.findByIdOrNull(id) } throws IllegalArgumentException("test iae")
+
+        val result = pollService.updatePoll(request, id)
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty())
+    }
+
+    @Test
+    fun updatePollDataInaccessible() {
+        val id = UUID.randomUUID()
+        val request = PollRequest(
+            name = "new test poll",
+            description = "new description for test poll",
+            ownerId = UUID.randomUUID(),
+            startTime = Instant.now(),
+            endTime = Instant.now().plus(5, ChronoUnit.DAYS),
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        every { pollRepo.findByIdOrNull(id) } throws DataRetrievalFailureException("test dae")
+
+        val result = pollService.updatePoll(request, id)
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.statusCode)
+        assertTrue(result.body.isNullOrEmpty())
+    }
+
+    @Test
+    fun updatePollPastScheduled() {
+        val ownerId = UUID.randomUUID()
+        val ownerIdNew = UUID.randomUUID()
+        val id = UUID.randomUUID()
+        val startTime = Instant.now()
+        val endTime = Instant.now().plus(4, ChronoUnit.DAYS)
+        val endTimeNew = endTime.plus(1, ChronoUnit.DAYS)
+        val request = PollRequest(
+            name = "new test poll",
+            description = "new description for test poll",
+            ownerId = ownerIdNew,
+            startTime = null,
+            endTime = endTimeNew,
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        val existingPoll = Poll(
+            id = id,
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = ownerId,
+            status = PollStatus.Active,
+            startTime = startTime,
+            endTime = endTime,
+            options = emptyList()
+        )
+        every { pollRepo.findByIdOrNull(id) } returns existingPoll
+
+        val result = pollService.updatePoll(request, id)
+
+        assertEquals(HttpStatus.FORBIDDEN, result.statusCode)
+        assertTrue(result.body!!.isNotBlank())
+    }
+
+    @Test
+    fun updatePollInvalidName() {
+        val ownerId = UUID.randomUUID()
+        val ownerIdNew = UUID.randomUUID()
+        val id = UUID.randomUUID()
+        val startTime = Instant.now()
+        val endTime = Instant.now().plus(4, ChronoUnit.DAYS)
+        val endTimeNew = endTime.plus(1, ChronoUnit.DAYS)
+        val request = PollRequest(
+            name = "", // name cannot be empty
+            description = "new description for test poll",
+            ownerId = ownerIdNew,
+            startTime = null,
+            endTime = endTimeNew,
+            options = listOf(
+                PollOption("new opt1", "desc1"),
+                PollOption("new opt2", "desc2")
+            )
+        )
+        val existingPoll = Poll(
+            id = id,
+            name = "test poll",
+            description = "description for test poll",
+            ownerId = ownerId,
+            status = PollStatus.Scheduled,
+            startTime = startTime,
+            endTime = endTime,
+            options = emptyList()
+        )
+        every { pollRepo.findByIdOrNull(id) } returns existingPoll
+
+        val result = pollService.updatePoll(request, id)
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.statusCode)
+        assertTrue(result.body.isNullOrBlank())
     }
 }
